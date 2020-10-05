ad815676dc43ae2964f62f940f2501c08aef2585
Moulynette <72034885+Moulynette@users.noreply.github.com>
2020-10-05 17:38:40 +0200

    Add files via upload

---
+++ PrinciplesOfPrograming/Lessons1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Lessons1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //test
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
